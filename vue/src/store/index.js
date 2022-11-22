57af603afcb8ed2e7c28f3800233355ca4112c1a
Ali Abodaraa <abodaraaali50@gmail.com>
2022-11-22 22:54:46 +0300

    get all serveys from DB

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,261 +1,7 @@
 import axios from "axios";
 import { createStore } from "vuex";
 import axiosClient from "../axios";
-const tmpSurveys = [{
-    id: 100,
-    title: "servey 1",
-    slug: "slug 1",
-    status: "status 1",
-    image: "https://via.placeholder.com/728x90.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat nemo ipsam magnam voluptatem repellendus id rem molestiae dolorum deserunt placeat aperiam voluptate obcaecati incidunt voluptates temporibus ducimus, corrupti delectus voluptatum?    ",
-    created_at: "2022-09-10 12:21:23",
-    updated_at: "2022-11-03 11:23:23",
-    expire_date: "2022-09-12 10:43:23",
-    questions: [{
-        id: 1,
-        type: "select",
-        question: "From which you are ?",
-        description: "ds",
-        data: {
-            options: [{
-                uuid: "dasfsdftnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "USA"
-            }, {
-                uuid: "$2y$10$f/sdgd.jyuk",
-                text: "Georgia"
-            }, {
-                uuid: "$$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "syria"
-            }, {
-                uuid: "10$M46qYAWnY6fK/xUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "RFA"
-            }, {
-                uuid: "$sdg$10$sdfsdf/xsgUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "TPO"
-            }, {
-                uuid: "$sdf$10$sdf/xUdatnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "BZM"
-            }]
-        }
-    }, {
-        id: 2,
-        type: "checkbox",
-        question: "How are you ?",
-        description: "So I don't get it, is this a laravel 5.3 bug or something? This is my php artisan route list",
-        data: {
-            options: [{
-                uuid: "pii676.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "laravel 5"
-            }, {
-                uuid: "$yuiyu$10$f/sdgd.jyuk",
-                text: "laravel 6"
-            }, {
-                uuid: "7896nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "laravel 7"
-            }, {
-                uuid: "10$678/xUdatnq2eRwB.$2y$70$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "laravel 8"
-            }, {
-                uuid: "$678$10$457/69.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "laravel 9"
-            }, {
-                uuid: "567PAMn9gIhjrGmR5jyQs3KaGS",
-                text: "TypeScript"
-            }]
-        }
-    }, {
-        id: 3,
-        type: "checkbox",
-        question: "where are you ?",
-        description: "Display - Tailwind CSS, Layout, Display, Utilities for controlling the display box type of an element. Basic usage, Block & Inline, Use inline, inline-block, and block to control the flow of text and elements. When controlling the flow of text, using the CSS property display: inline will cause",
-        data: {
-            options: [{
-                uuid: "dasfsdftnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "tailwindcss"
-            }, {
-                uuid: "$2y$10$f/sdgd.jyuk",
-                text: "java"
-            }, {
-                uuid: "$$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "c++"
-            }, {
-                uuid: "10$M46qYAWnY6fK/xUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "HTML"
-            }, {
-                uuid: "$sdg$10$sdfsdf/xsgUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "XML"
-            }, {
-                uuid: "$sdf$10$sdf/xUdatnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "PHP"
-            }]
-        }
-    }, {
-        id: 4,
-        type: "seclect",
-        question: "what is the favorite hoppy ?",
-        description: "So I don't get it, is this a laravel 5.3 bug or something? This is my php artisan route list",
-        data: {
-            options: [{
-                uuid: "pii676.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "Audi"
-            }, {
-                uuid: "$yuiyu$10$f/sdgd.jyuk",
-                text: "Kia"
-            }, {
-                uuid: "7896nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "Saba"
-            }, {
-                uuid: "10$678/xUdatnq2eRwB.$2y$70$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "Toyota"
-            }, {
-                uuid: "$678$10$457/69.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "BMW"
-            }, {
-                uuid: "567PAMn9gIhjrGmR5jyQs3KaGS",
-                text: "Mercides"
-            }]
-        }
-    }, {
-        id: 5,
-        type: "text",
-        question: "what's your favourite youtube channel ?",
-        description: "Display - Tailwind CSS, Layout, Display, Utilities for controlling the display box type of an element. Basic usage, Block & Inline, Use inline, inline-block, and block to control the flow of text and elements. When controlling the flow of text, using the CSS property display: inline will cause",
-        data: {}
-    }, {
-        id: 6,
-        type: "textarea",
-        question: "what's do yo think about w3c youtube channel ?",
-        description: "write your honest option. Everything is anonymous",
-        data: {}
-    }],
-}, {
-    id: 101,
-    title: "servey 2",
-    slug: "slug 2",
-    status: "status 2",
-    image: "https://via.placeholder.com/728x90.png",
-    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat nemo ipsam magnam voluptatem repellendus id rem molestiae dolorum deserunt placeat aperiam voluptate obcaecati incidunt voluptates temporibus ducimus, corrupti delectus voluptatum?    ",
-    created_at: "2022-09-10 12:21:23",
-    updated_at: "2022-11-03 11:23:23",
-    expire_date: "2022-09-12 10:43:23",
-    questions: [{
-        id: 1,
-        type: "select",
-        question: "From which you are ?",
-        description: null,
-        data: {
-            options: [{
-                uuid: "dasfsdftnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "USA"
-            }, {
-                uuid: "$2y$10$f/sdgd.jyuk",
-                text: "Georgia"
-            }, {
-                uuid: "$$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "syria"
-            }, {
-                uuid: "10$M46qYAWnY6fK/xUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "RFA"
-            }, {
-                uuid: "$sdg$10$sdfsdf/xsgUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "TPO"
-            }, {
-                uuid: "$sdf$10$sdf/xUdatnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "BZM"
-            }]
-        }
-    }, {
-        id: 2,
-        type: "checkbox",
-        question: "How are you ?",
-        description: "So I don't get it, is this a laravel 5.3 bug or something? This is my php artisan route list",
-        data: {
-            options: [{
-                uuid: "pii676.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "laravel 5"
-            }, {
-                uuid: "$yuiyu$10$f/sdgd.jyuk",
-                text: "laravel 6"
-            }, {
-                uuid: "7896nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "laravel 7"
-            }, {
-                uuid: "10$678/xUdatnq2eRwB.$2y$70$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "laravel 8"
-            }, {
-                uuid: "$678$10$457/69.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "laravel 9"
-            }, {
-                uuid: "567PAMn9gIhjrGmR5jyQs3KaGS",
-                text: "TypeScript"
-            }]
-        }
-    }, {
-        id: 3,
-        type: "checkbox",
-        question: "where are you ?",
-        description: "Display - Tailwind CSS, Layout, Display, Utilities for controlling the display box type of an element. Basic usage, Block & Inline, Use inline, inline-block, and block to control the flow of text and elements. When controlling the flow of text, using the CSS property display: inline will cause",
-        data: {
-            options: [{
-                uuid: "dasfsdftnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "tailwindcss"
-            }, {
-                uuid: "$2y$10$f/sdgd.jyuk",
-                text: "java"
-            }, {
-                uuid: "$$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "c++"
-            }, {
-                uuid: "10$M46qYAWnY6fK/xUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "HTML"
-            }, {
-                uuid: "$sdg$10$sdfsdf/xsgUdatnq2eRwB.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "XML"
-            }, {
-                uuid: "$sdf$10$sdf/xUdatnq2eRwB.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "PHP"
-            }]
-        }
-    }, {
-        id: 4,
-        type: "seclect",
-        question: "what is the favorite hoppy ?",
-        description: "So I don't get it, is this a laravel 5.3 bug or something? This is my php artisan route list",
-        data: {
-            options: [{
-                uuid: "pii676.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "Audi"
-            }, {
-                uuid: "$yuiyu$10$f/sdgd.jyuk",
-                text: "Kia"
-            }, {
-                uuid: "7896nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6$10$/.lq8JPAMn9gIhjrGmR5jyQs3KaGS",
-                text: "Saba"
-            }, {
-                uuid: "10$678/xUdatnq2eRwB.$2y$70$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "Toyota"
-            }, {
-                uuid: "$678$10$457/69.$2y$10$6nXov8BW4J29TfKXMExAXewf2vixaIHNHbOLFgEkuoezLDFIuZbb6",
-                text: "BMW"
-            }, {
-                uuid: "567PAMn9gIhjrGmR5jyQs3KaGS",
-                text: "Mercides"
-            }]
-        }
-    }, {
-        id: 5,
-        type: "text",
-        question: "what's your favourite youtube channel ?",
-        description: "Display - Tailwind CSS, Layout, Display, Utilities for controlling the display box type of an element. Basic usage, Block & Inline, Use inline, inline-block, and block to control the flow of text and elements. When controlling the flow of text, using the CSS property display: inline will cause",
-        data: {}
-    }, {
-        id: 6,
-        type: "textarea",
-        question: "what's do yo think about w3c youtube channel ?",
-        description: "write your honest option. Everything is anonymous",
-        data: {}
-    }],
-}];
+
 const store = createStore({
     state: {
         user: {
@@ -276,7 +22,10 @@ const store = createStore({
 
             }
         },
-        surveys: [...tmpSurveys],
+        surveys: {
+            loading: false,
+            data: [],
+        },
         questionTypes: ["text", "select", "radio", "checkbox", "textarea"],
     },
     getters: {
@@ -309,25 +58,18 @@ const store = createStore({
             sessionStorage.setItem("name", userDataRes.data.user.name);
             sessionStorage.setItem("TOKEN", userDataRes.data.token); //save token that comes from backend in a session storage
         },
-        // saveSurvey(state, survey) {
-        //     state.surveys = [...state.surveys, survey.data];
-        //     console.log(state.surveys);
-
-        // },
-        // updateSurvey(state, survey) {
-        //     console.log(survey);
-        //     state.surveys = state.surveys.map((sur) => {
-        //         if (sur.id == survey.data.id) {
-        //             return survey.data;
-        //         }
-        //         return sur;
-        //     });
-        // },
         setCurrentSurvey(state, survey) {
             state.currentSurvey.data = survey.data;
         },
         setCurrentSurveyLoading(state, loading) {
             state.currentSurvey.loading = loading;
+        },
+        setSurveys(state, surveys) {
+            //debugger;
+            state.surveys.data = surveys.data;
+        },
+        setSurveysLoading(state, loading) {
+            state.surveys.loading = loading;
         }
     },
     actions: {
@@ -382,7 +124,7 @@ const store = createStore({
             }
             return response;
         },
-        getSurvey({ commit }, id) {
+        getSurveys({ commit }, id) {
             console.log("LOADING ....");
             commit("setCurrentSurveyLoading", true);
             return axiosClient.get(`/survey/${id}`)
@@ -400,6 +142,15 @@ const store = createStore({
         deleteSurvey({}, id) {
             return axiosClient.delete(`/survey/${id}`);
         },
+        getSurveys({ commit }) {
+            commit('setSurveysLoading', true);
+            return axiosClient.get('/survey').then((res) => {
+                commit('setSurveysLoading', false);
+                commit('setSurveys', res.data);
+                console.log(res.data.data);
+                return res;
+            });
+        }
     },
     modules: {
 
--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -15,7 +15,7 @@
       <div v-if="surveys" class="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 mt-5 bg-dark-400">
         <div v-for="survey in surveys" :key="survey.id" 
         class="flex flex-col py-4 px-6 shadow-md bg-white hover:bg-grey-50 h-470px] bg-floralwhite-100">
-          <img :src="survey.image" alt="" class="w-full h-48 object-cover">
+          <img :src="survey.image_url" alt="" class="w-full h-48 object-cover">
           <h4 class="mt-4 text-lg font-bold">{{survey.title}}</h4>
           <div v-html="survey.description" class="overflow-hidden flex-1"></div>
           <hr class="m-4">
@@ -50,8 +50,7 @@
      import PageComponents from "../components/PageComponents.vue";
      import store from "../store";
      import { computed } from "vue";
-    //const surveys=computed(()=>{return store.state.surveys}) //it is true but why we use computed
-     const surveys=store.state.surveys; //also true
+    const surveys=computed(()=>{return store.state.surveys.data;}) //it is true but why we use computed
 
      console.log(surveys)
      function deleteServey(){
@@ -60,6 +59,10 @@
       }
      }
      
+
+     //get all surveys from DB
+     
+     store.dispatch('getSurveys');
      </script>
     <style scoped>
     
